26d54172f38d0c80e37252d6a65f80f4af898d53
gauravmishra193 <55256933+gauravmishra193@users.noreply.github.com>
2022-05-29 19:17:05 +0530

    Update 27.js

--- part1/27.js
+++ part1/27.js
@@ -4,20 +4,26 @@
 
 let fruits = ["apple", "mango", "grapes"];
 console.log(fruits);
-// push 
+
+// push --> will push in the last;
 // fruits.push("banana");
+
 // console.log(fruits);
-// pop 
+
+// pop --> pop from the last. POP will not only return but will also RETURN and hence can be stored in a variable.
+
 // let poppedFruit = fruits.pop();
 // console.log(fruits);
 // console.log("popped fruits is", poppedFruit);
 
-// unshift 
+// unshift --> will push an element in strting.
 // fruits.unshift("banana");
 // fruits.unshift("myfruit");
 // console.log(fruits);
 
-// shift 
+// shift --> remove from starting.
 // let removedFruit = fruits.shift();
 // console.log(fruits);
 // console.log("removed fruits is ", removedFruit);
+
+//  PUSH/POP oparations are faser than SHIFT/UNSHIFT.
