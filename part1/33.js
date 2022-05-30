0c2dcfc4fc9bdba8611334de93ac61b3db54a063
gauravmishra193 <55256933+gauravmishra193@users.noreply.github.com>
2022-05-30 13:21:06 +0530

    Update 33.js
    for-if loop

--- part1/33.js
+++ part1/33.js
@@ -3,7 +3,7 @@
 const fruits = ["apple", "mango", "grapes", "fruit4", "fruit5"];
 const fruits2 = [];
 
-// for(let fruit of fruits){ --------->>> the loop will iterate for as many times as the no. of elements in the fruitS array, and copy those elements in fruiT array.
+// for(let fruit of fruits){ --------->>> the loop will iterate for as many times as the no. of elements in the fruitS array, and copy those elements in fruit array.
 //     fruits2.push(fruit.toUpperCase());--------> console.log(fruit.toUpperCase()) will make the elements in CAPITAL LETTERS and then PRINT it;
 // and fruit2.push will push the capital letter elements in the fruit2 array.
 // }
