cab3a2176ed65047b5988e457848342f727ecbf8
gauravmishra193 <55256933+gauravmishra193@users.noreply.github.com>
2022-05-30 18:51:28 +0530

    Update 36_Objects.js

--- part1/36_Objects.js
+++ part1/36_Objects.js
@@ -6,19 +6,27 @@
 
 // how to create objects 
 
-// const person = {name:"Harshit",age:22};
+// const person = {name:"Harshit",age:22};--->> here name is key and harshit is its value. Similarly age and 22.
+// when array is declared in this way i.e using curly braces and all, then it is called object Literals.
 const person = {
     name: "harshit",
     age: 22,
-    hobbies: ["guitar", "sleeping", "listening music"]
+    hobbies: ["guitar", "sleeping", "listening music"]--->> // object can store an array also
 }
-console.log(person);
 
+// Accesing the data of an object is done using dots.
+    console.log(person.name);
+// Accesing the data of an object is done using square brackets.
+    console.log(person["name"] // double invCommas are used because in objects the keys are by default stored as string only.
+
+   
 // how to access data from objects 
 // console.log(person["name"]);
 // console.log(person["age"]);
 // console.log(person.hobbies);
 
 // how to add key value pair to objects
+person.gender="male" // now another key value pair has been added in the object Person.
+
 person["person"] = "male";
 console.log(person);
