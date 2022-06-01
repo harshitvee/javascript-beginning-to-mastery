867dd912f0ef7b57ea2a27e350142c7405e862cd
gauravmishra193 <55256933+gauravmishra193@users.noreply.github.com>
2022-06-01 12:21:41 +0530

    Update 38.js

--- part1/38.js
+++ part1/38.js
@@ -9,11 +9,15 @@ There are 2 ways to iterate objects:
 //  1. using for-in loop
 //  2. using Object.keys 
 
-//  1. using for-in loop
+//  1. using for-in loop ===> using for-in loop for object will store the 'keys' of the object in the variable 'key' or whatever var is used.
     // for(let key in person){
-    //     // console.log(`${key} : ${person[key]}`);
-    //     console.log(key," : " ,person[key]);
-    // }
+        // console.log(key); ===> this will print all the keys.
+        // }
+
+    // for(let key in person){
+        // console.log(`${key} : ${person[key]}`);
+        //  console.log(key," : " ,person[key]);
+        // }
 
 //  2. using Object.keys    
     // console.log(typeof (Object.keys(person)));
