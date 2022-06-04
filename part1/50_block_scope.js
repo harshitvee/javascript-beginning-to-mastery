e4862a6dd559c20b2af92e407d79202454129313
gauravmishra193 <55256933+gauravmishra193@users.noreply.github.com>
2022-06-04 17:27:16 +0530

    Update 50_block_scope.js

--- part1/50_block_scope.js
+++ part1/50_block_scope.js
@@ -3,18 +3,18 @@
 
 WHAT IS A BLOCK SCOPE?
     1{
-            This is one block(from 1 to 2).
+       2{
+            This is one block(from 2 to 3).
             Any var declared here can be accesed here only in case of let and const because let and const are block scope. 
             B ut a variable can be accessed outside the block if declared using var becasue var is a function scope.
             let and const var will be accesse from 1 to 2 only            
-    2}   
+        3}   
     
-    3{
-            This is another block(from 3 to 4)
-            
-    4}
-
-    But any var declared using var can be accesse between and beyond 1 and 4.
+        4{
+            This is another block(from 4 to 5)            
+        5}
+    6}
+    But any variable declared using var can be accesse between and beyond 1 and 6, but not beyond 6.
 
 
 
