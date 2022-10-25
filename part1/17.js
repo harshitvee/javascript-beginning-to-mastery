ef7f9c2f751b4c002f26223ca098b25d86fad653
iamfutureflash <xenode2000@gmail.com>
2022-10-25 17:41:41 +0530

    done lec 32 part1

--- part1/17.js
+++ part1/17.js
@@ -5,12 +5,16 @@
 // 19 your guess is right 
 // 17 too low 
 // 20 too high 
-
+// let k= true;
 let winningNumber = 19;
+// while(k){
 let userGuess = +prompt("Guess a number");
 
 if(userGuess === winningNumber){
     console.log("Your guess is right!!");
+    console.log("ok bye");
+    // k=false;
+    // break;
 }else{
     if(userGuess < winningNumber){
         console.log("too low !!!");
